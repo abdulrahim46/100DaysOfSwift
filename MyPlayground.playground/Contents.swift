4ace3ed3b993596932e3ead771e75a90307a254e
Abdul chaudhary <abdul.chaudhary@peoplestrong.com>
2020-06-14 16:37:19 +0530

    initial commit2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -12,3 +12,12 @@ lets see this
 
 var pi = 3.141414
 var cf = true
+
+//string interpolation
+
+var score = 85
+var str2 = "your score on math test is \(score)"
+
+var results = "the test results are here: \(str2)"
+
+//As you’ll see later on, string interpolation isn’t just limited to placing variables – you can actually run code inside there.
